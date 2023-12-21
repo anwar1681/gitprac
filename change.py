f0b8c1d9995e83b87777e6aa8c554ec147c3a61f
=Anwar <anwar1681@yahoo.com>
2023-12-21 18:07:46 +0500

    some change in file

--- change.py
+++ change.py
@@ -1,12 +1,12 @@
 def changetemp():
-    temp = input('enter temprature  f or c: ')
-    if temp == f:
+    temp = input('enter temprature f or c: ')
+    if temp == 'f':
         F = int(input('enter temperature if farenheit: '))
-        c = (F-32)*5/9
+        c = round((F-32)*5/9,2)
         print(f'The centi equivalent of {F} F is: {c} C')
-    elif temp == c:
+    elif temp == 'c':
         C = int(input('temperature if centigrade: '))
-        f = C*9/5 + 32
+        f = round(C*9/5 + 32,2)
         print(f'The farenheit equivalent of {C} C is: {f} F')
     else:
         print('enter temp f or c')
